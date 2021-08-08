bf4c24bd7c7f8c7fdbe6af2a5fbf9b0251aef66e
Keyno <kofujiw@gmail.com>
2021-08-08 22:04:19 +0900

    sample code 修正

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import {createApiData} from "./request";
+import {getIssueData, getPRData} from "./request";
 
 function hello(name: string): string {
     return `Hello, ${name}!`;
@@ -6,4 +6,26 @@ function hello(name: string): string {
 
 console.log(hello("TypeScript"));
 
-createApiData(3).then(v => console.log(v));
+const dt = new Date()
+dt.setDate(dt.getDate() - 7)
+
+// 一週間以内に終了した  一覧をとる
+getPRData(20)
+    .then(issues => {
+        const ret = issues
+            .filter(issue => issue.state === "closed" && issue.closed_at > dt.getTime())
+
+        console.log("closed recent prs.")
+        console.log(ret)
+    })
+
+// 一週間以内に終了した Issue 一覧をとる
+getIssueData(20)
+    .then(issues => {
+        const ret = issues
+            .filter(issue => issue.state === "closed" && issue.closed_at > dt.getTime())
+
+        console.log("closed recent issues.")
+        console.log(ret)
+    })
+
--- src/request.ts
+++ src/request.ts
@@ -41,7 +41,7 @@ function dateParseChallenge(key: string, val: any) {
     return val;
 }
 
-type GithubApiData2 = {
+type GithubApiPRResponse = {
     id: number
     user: GithubUser
     state: string
@@ -51,6 +51,20 @@ type GithubApiData2 = {
     updated_at: string
     closed_at: string
     merged_at: string
+}
+
+type GithubApiIssueResponse = {
+    id: number
+    user: GithubUser
+    assignee: string
+    state: string
+    url: string
+    title: string
+    labels: Array<Label>
+    created_at: string
+    updated_at: string
+    closed_at: string
+    pull_request: Object
 }
 
 class GithubApiPRData {
@@ -77,8 +91,42 @@ class GithubApiPRData {
     }
 }
 
+class GithubApiIssueData {
+    id: number
+    user: string
+    assignee: string
+    state: string
+    url: string
+    title: string
+    labels: Array<string>
+    created_at: number
+    updated_at: number
+    closed_at: number
+
+    constructor(id: number, user: string, assignee: string, state: string, url: string, title: string, labels: Array<string>, created_at: number, updated_at: number, closed_at: number) {
+        this.id = id;
+        this.user = user;
+        this.assignee = assignee;
+        this.state = state;
+        this.url = url;
+        this.title = title;
+        this.labels = labels;
+        this.created_at = created_at;
+        this.updated_at = updated_at;
+        this.closed_at = closed_at;
+    }
+}
+
 type GithubUser = {
     login: string
+}
+
+class Label {
+    name: string
+
+    constructor(name: string) {
+        this.name = name;
+    }
 }
 
 enum State {
@@ -87,30 +135,66 @@ enum State {
     Other = ''
 }
 
+const orgName = "ghostdogpr"
+const repoName = "caliban"
+
 // method
-export const createApiData = async (pageNum: number = 1) => {
-    const response: AxiosResponse = await apiClient.get<Array<GithubApiData2>>('/ghostdogpr/caliban/pulls', {
+export const getPRData = async (pageNum: number = 1) => {
+    const response: AxiosResponse = await getGithubApi<GithubApiPRResponse>(orgName, repoName, 'pulls', pageNum)
+
+    return convertPRData(response.data)
+}
+
+export const getIssueData = async (pageNum: number = 1) => {
+    const response: AxiosResponse = await getGithubApi<GithubApiIssueResponse>(orgName, repoName, 'issues', pageNum)
+
+    return convertIssueData(response.data)
+}
+
+async function getGithubApi<T = any>(org: string, repo: string, path: string, pageNum: number = 1) {
+    const paths = `/${org}/${repo}/${path}`
+    const response: AxiosResponse = await apiClient.get<Array<T>>(paths, {
         params: {
             per_page: pageNum,
             state: "closed"
         }
     });
-
-    return change(response.data)
+    return response
 }
 
-const change = (data: Array<GithubApiData2>) => {
+const convertPRData = (data: Array<GithubApiPRResponse>) => {
     return data.map(pr => {
         return new GithubApiPRData(
             pr.id,
-            pr.state,
             pr.user.login,
+            pr.state,
             pr.url,
             pr.title,
             convertToTime(pr.created_at),
             convertToTime(pr.updated_at),
             convertToTime(pr.closed_at),
             convertToTime(pr.merged_at)
+        )
+    })
+}
+
+const convertIssueData = (data: Array<GithubApiIssueResponse>) => {
+    return data.filter(issue => issue.pull_request === undefined)
+        .map(issue => {
+        //console.log(issue);
+        const labels = issue.labels
+            .map(l => l.name)
+        return new GithubApiIssueData(
+            issue.id,
+            issue.user.login,
+            issue.assignee,
+            issue.state,
+            issue.url,
+            issue.title,
+            labels,
+            convertToTime(issue.created_at),
+            convertToTime(issue.updated_at),
+            convertToTime(issue.closed_at)
         )
     })
 }
