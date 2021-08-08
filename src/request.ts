6c62b4154e666ab1ea391db74be2c571bbfb94f7
Keyno <kofujiw@gmail.com>
2021-08-08 14:13:58 +0900

    request まわりの修正

--- src/request.ts
+++ src/request.ts
@@ -43,32 +43,57 @@ function dateParseChallenge(key: string, val: any) {
 
 type GithubApiData2 = {
     id: number
+    user: GithubUser
+    state: string
     url: string
     title: string
     created_at: string
     updated_at: string
+    closed_at: string
+    merged_at: string
 }
 
-class GithubApiData3 {
+class GithubApiPRData {
     id: number
+    user: string
+    state: string
     url: string
     title: string
     created_at: number
     updated_at: number
+    closed_at: number
+    merged_at: number
 
-    constructor(id: number, url: string, title: string, created_at: number, updated_at: number) {
+    constructor(id: number, user: string, state:string, url: string, title: string, created_at: number, updated_at: number, closed_at: number, merged_at: number) {
         this.id = id;
+        this.user = user;
+        this.state = state;
         this.url = url;
         this.title = title;
         this.created_at = created_at;
         this.updated_at = updated_at;
+        this.closed_at = closed_at;
+        this.merged_at = merged_at;
     }
+}
+
+type GithubUser = {
+    login: string
+}
+
+enum State {
+    Closed = 'closed',
+    Open = 'open',
+    Other = ''
 }
 
 // method
 export const createApiData = async (pageNum: number = 1) => {
     const response: AxiosResponse = await apiClient.get<Array<GithubApiData2>>('/ghostdogpr/caliban/pulls', {
-        params: { per_page: pageNum}
+        params: {
+            per_page: pageNum,
+            state: "closed"
+        }
     });
 
     return change(response.data)
@@ -76,12 +101,20 @@ export const createApiData = async (pageNum: number = 1) => {
 
 const change = (data: Array<GithubApiData2>) => {
     return data.map(pr => {
-        return new GithubApiData3(
+        return new GithubApiPRData(
             pr.id,
+            pr.state,
+            pr.user.login,
             pr.url,
             pr.title,
-            new Date(pr.created_at).getTime(),
-            new Date(pr.updated_at).getTime()
+            convertToTime(pr.created_at),
+            convertToTime(pr.updated_at),
+            convertToTime(pr.closed_at),
+            convertToTime(pr.merged_at)
         )
     })
+}
+
+const convertToTime = (value: string) => {
+    return new Date(value).getTime()
 }
